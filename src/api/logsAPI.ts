1ea4e0eeffde20086891debfe56868173cc88dc9
42inshin <inshin@student.42seoul.kr>
2023-02-21 00:30:12 +0900

    FIX: axios async await 추가

--- src/api/logsAPI.ts
+++ src/api/logsAPI.ts
@@ -13,22 +13,24 @@ export type InOutLog = {
 };
 
 const getLogsDayURL = "v1/tag-log/perday";
-export const getLogsDay = (year: number, month: number, day: number) => {
-  return instance.get(getLogsDayURL, {
+export const getLogsDay = async (year: number, month: number, day: number) => {
+  const response = await instance.get(getLogsDayURL, {
     params: {
       year,
       month,
       day,
     },
   });
+  return response;
 };
 
 const getLogsMonthURL = "v1/tag-log/permonth";
-export const getLogsmonth = (year: number, month: number) => {
-  return instance.get(getLogsMonthURL, {
+export const getLogsmonth = async (year: number, month: number) => {
+  const response = await instance.get(getLogsMonthURL, {
     params: {
       year,
       month,
     },
   });
+  return response;
 };
--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -2,16 +2,19 @@ import { instance } from "api/baseAPI";
 import { UserInfoResponse } from "types/User";
 
 const isLoginURL = "user/login/isLogin";
-export const getIsLogin = () => {
-  return instance.get(isLoginURL);
+export const getIsLogin = async () => {
+  const response = await instance.get(isLoginURL);
+  return response;
 };
 
 const userInfoURL = "v1/tag-log/maininfo";
-export const getUserInfo = () => {
-  return instance.get<UserInfoResponse>(userInfoURL);
+export const getUserInfo = async () => {
+  const response = await instance.get<UserInfoResponse>(userInfoURL);
+  return response;
 };
 
 const accTimesURL = "v1/tag-log/accumulationTimes";
-export const getAccTimes = () => {
-  return instance.get(accTimesURL);
+export const getAccTimes = async () => {
+  const response = await instance.get(accTimesURL);
+  return response;
 };
